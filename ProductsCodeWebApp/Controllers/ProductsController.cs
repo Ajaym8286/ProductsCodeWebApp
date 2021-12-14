8ba61ab194651a2c7178ac9a60ab8b74e6b5472e
Ajay <Ajay@ASUS-PC>
2021-12-14 23:43:41 +0530

    Add project files.

---
+++ ProductsCodeWebApp/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProductsCodeWebApp.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ProductsCodeWebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index(ProductService dr)
+        {
+            string mainconn = ConfigurationManager.ConnectionStrings["Myconn"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            string s1 = "Select * from [dbo].[deltaproducts]";
+            SqlCommand sqlcomm = new SqlCommand(s1);
+            sqlcomm.Connection = sqlconn;
+            sqlconn.Open();
+            SqlDataReader sdr = sqlcomm.ExecuteReader();
+            List<ProductService> objmodel = new List<ProductService>();
+            if (sdr.HasRows)
+            {
+                while(sdr.Read())
+                {
+                    var details = new ProductService();
+                    details.Product_ID = sdr["Product_ID"].ToString();
+                    details.Name = sdr["Name"].ToString();
+                    details.Description = sdr["Description"].ToString();
+                    objmodel.Add(details);
+                }
+                dr.ProductsDetails = objmodel;
+                sqlconn.Close();
+
+            }
+            return View("Index",dr);
+        }
+    }
+}
---
+++ ProductsCodeWebApp/Models/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductsCodeWebApp.Models
+{
+    public class ProductService
+    {
+        public string Product_ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public List<ProductService> ProductsDetails { get; set; }
+
+
+    }
+}
